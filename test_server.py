040fd1be26b2953ebd0a35f653b4bab4da33da7e
Aviad Moreshet <aviad10m@gmail.com>
2018-06-08 13:33:24 +0300

    renamed some funcs in integration tests

--- test_server.py
+++ test_server.py
@@ -18,10 +18,10 @@ class ServerTestCase(unittest.TestCase):
     def test_initial_state_calculatorstate_null(self):
         _state = None
         _input = "1"
-        body = self.generateBody(_state, _input)
-        resp = self.postCalculate(self, body)
-        self.validateNextStateResponse(resp)
-        data = self.getResponseData(resp)
+        body = self.generate_body(_state, _input)
+        resp = self.post_calculate(self, body)
+        self.validate_next_state_response(resp)
+        data = self.get_response_data(resp)
         assert data['display'] == _input
         assert data['stack'][0] == _input
 
@@ -30,28 +30,28 @@ class ServerTestCase(unittest.TestCase):
         inputs = ["1", "2", "3"]
 
         # First request
-        body = self.generateBody(_state, inputs[0])
-        resp = self.postCalculate(self, body)
-        state = self.getResponseData(resp)
+        body = self.generate_body(_state, inputs[0])
+        resp = self.post_calculate(self, body)
+        state = self.get_response_data(resp)
 
         # Second request
-        body = self.generateBody(state, inputs[1])
-        resp = self.postCalculate(self, body)
+        body = self.generate_body(state, inputs[1])
+        resp = self.post_calculate(self, body)
 
         # Validate second request
-        self.validateNextStateResponse(resp)
-        state = self.getResponseData(resp)
+        self.validate_next_state_response(resp)
+        state = self.get_response_data(resp)
         assert state['display'] == ''.join(inputs[:2])
         assert state['stack'][0] == inputs[0]
         assert state['stack'][1] == inputs[1]
 
         # Third request
-        body = self.generateBody(state, inputs[2])
-        resp = self.postCalculate(self, body)
+        body = self.generate_body(state, inputs[2])
+        resp = self.post_calculate(self, body)
 
         # Validate third request
-        self.validateNextStateResponse(resp)
-        state = self.getResponseData(resp)
+        self.validate_next_state_response(resp)
+        state = self.get_response_data(resp)
         assert state['display'] == ''.join(inputs[:3])
         assert state['stack'][0] == inputs[0]
         assert state['stack'][1] == inputs[1]
@@ -60,12 +60,12 @@ class ServerTestCase(unittest.TestCase):
     # def test_plus_op(self):
     #     _state = None
     #     _input = "1"
-    #     initialStateBody = self.generateBody(_state, _input)
-    #     resp = self.postCalculate(self, initialStateBody)
+    #     initialStateBody = self.generate_body(_state, _input)
+    #     resp = self.post_calculate(self, initialStateBody)
     #
     #
-    #     self.validateNextStateResponse(resp)
-    #     data = self.getResponseData(resp)
+    #     self.validate_next_state_response(resp)
+    #     data = self.get_response_data(resp)
     #     assert data['display'] == _input
     #     assert data['stack'][0] == _input
 
@@ -73,10 +73,10 @@ class ServerTestCase(unittest.TestCase):
     def test_should_return_invalid_op_initial_state_input_is_eq(self):
         _state = None
         _input = "="
-        body = self.generateBody(_state, _input)
-        resp = self.postCalculate(self, body)
-        self.validateNextStateResponse(resp)
-        data = self.getResponseData(resp)
+        body = self.generate_body(_state, _input)
+        resp = self.post_calculate(self, body)
+        self.validate_next_state_response(resp)
+        data = self.get_response_data(resp)
         assert data['display'] == INVALID_OP_STRING
         assert data['is_invalid_input'] is False
         assert data['is_operator_in_stack'] is False
@@ -86,10 +86,10 @@ class ServerTestCase(unittest.TestCase):
     def test_should_return_invalid_op_initial_state_input_is_mult(self):
         _state = None
         _input = "*"
-        body = self.generateBody(_state, _input)
-        resp = self.postCalculate(self, body)
-        self.validateNextStateResponse(resp)
-        data = self.getResponseData(resp)
+        body = self.generate_body(_state, _input)
+        resp = self.post_calculate(self, body)
+        self.validate_next_state_response(resp)
+        data = self.get_response_data(resp)
         assert data['display'] == INVALID_OP_STRING
         assert data['is_invalid_input'] is False
         assert data['is_operator_in_stack'] is False
@@ -99,10 +99,10 @@ class ServerTestCase(unittest.TestCase):
     def test_should_return_invalid_op_initial_state_input_is_plus(self):
         _state = None
         _input = "+"
-        body = self.generateBody(_state, _input)
-        resp = self.postCalculate(self, body)
-        self.validateNextStateResponse(resp)
-        data = self.getResponseData(resp)
+        body = self.generate_body(_state, _input)
+        resp = self.post_calculate(self, body)
+        self.validate_next_state_response(resp)
+        data = self.get_response_data(resp)
         assert data['display'] == INVALID_OP_STRING
         assert data['is_invalid_input'] is False
         assert data['is_operator_in_stack'] is False
@@ -112,10 +112,10 @@ class ServerTestCase(unittest.TestCase):
     def test_should_return_invalid_op_initial_state_input_is_minus(self):
         _state = None
         _input = "-"
-        body = self.generateBody(_state, _input)
-        resp = self.postCalculate(self, body)
-        self.validateNextStateResponse(resp)
-        data = self.getResponseData(resp)
+        body = self.generate_body(_state, _input)
+        resp = self.post_calculate(self, body)
+        self.validate_next_state_response(resp)
+        data = self.get_response_data(resp)
         assert data['display'] == INVALID_OP_STRING
         assert data['is_invalid_input'] is False
         assert data['is_operator_in_stack'] is False
@@ -125,10 +125,10 @@ class ServerTestCase(unittest.TestCase):
     def test_should_return_invalid_op_initial_state_input_is_unrecognized_char(self):
         _state = None
         _input = "$"
-        body = self.generateBody(_state, _input)
-        resp = self.postCalculate(self, body)
-        self.validateNextStateResponse(resp)
-        data = self.getResponseData(resp)
+        body = self.generate_body(_state, _input)
+        resp = self.post_calculate(self, body)
+        self.validate_next_state_response(resp)
+        data = self.get_response_data(resp)
         assert data['display'] == INVALID_OP_STRING
         assert data['is_invalid_input'] is False
         assert data['is_operator_in_stack'] is False
@@ -139,14 +139,14 @@ class ServerTestCase(unittest.TestCase):
         body = dict(
             input="1"
         )
-        resp = self.postCalculate(self, body)
+        resp = self.post_calculate(self, body)
         assert resp.status_code == 500
 
     def test_should_return_500_no_input_field(self):
         body = dict(
             calculaturState=None
         )
-        resp = self.postCalculate(self, body)
+        resp = self.post_calculate(self, body)
         assert resp.status_code == 500
 
     def test_should_return_500_input_is_not_string(self):
@@ -154,7 +154,7 @@ class ServerTestCase(unittest.TestCase):
             calculatorState=None,
             input=1
         )
-        resp = self.postCalculate(self, body)
+        resp = self.post_calculate(self, body)
         assert resp.status_code == 500
 
 
@@ -163,30 +163,30 @@ class ServerTestCase(unittest.TestCase):
             calculatorState="not_json",
             input=1
         )
-        resp = self.postCalculate(self, body)
+        resp = self.post_calculate(self, body)
         assert resp.status_code == 500
 
 
-    def validateNextStateResponse(self,resp):
+    def validate_next_state_response(self,resp):
         assert resp.status_code == 200
-        data = self.getResponseData(resp)
+        data = self.get_response_data(resp)
         assert 'display' in data and isinstance(data['display'], str)
         assert 'is_invalid_input' in data and isinstance(data['is_invalid_input'], bool)
         assert 'is_operator_in_stack' in data and isinstance(data['is_operator_in_stack'], bool)
         assert 'is_stack_head_a_result' in data and isinstance(data['is_stack_head_a_result'], bool)
         assert 'stack' in data and isinstance(data['stack'], list)
 
-    def getResponseData(self, resp):
+    def get_response_data(self, resp):
         return json.loads(resp.get_data(as_text=True))
 
-    def generateBody(self, state, _input):
+    def generate_body(self, state, _input):
         return dict(
             calculatorState=state,
             input=_input
         )
 
 
-    def postCalculate(self, instance, body):
+    def post_calculate(self, instance, body):
         return instance.app.post(
             ServerTestCase.CALCULATE_PATH,
             data=json.dumps(body),
